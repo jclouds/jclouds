b4b7f5623a5485eeefc6c9745b69e792644aab33
Ignasi Barrera <ignasi.barrera@abiquo.com>
2012-06-29 16:08:30 +0200

    Custom chef version also in Private Chef
    Configured PrivateChefAsyncClient to read the value of the
    X-Chef-Version header from the jclouds.api-version property.

--- apis/chef/src/main/java/org/jclouds/chef/ChefAsyncClient.java
+++ apis/chef/src/main/java/org/jclouds/chef/ChefAsyncClient.java
@@ -92,7 +92,7 @@ import com.google.common.util.concurrent.ListenableFuture;
 @Headers(keys = "X-Chef-Version", values = "{" + Constants.PROPERTY_API_VERSION + "}")
 @Consumes(MediaType.APPLICATION_JSON)
 public interface ChefAsyncClient {
-   public static final String VERSION = "0.9.8";
+   public static final String VERSION = "0.10.8";
 
    /**
     * @see ChefClient#getUploadSandboxForChecksums(Set)
