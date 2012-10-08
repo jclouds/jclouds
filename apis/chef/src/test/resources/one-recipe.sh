c6d57446e92ab9e2e7fbc70d2883c9adbb9f9a45
Ignasi Barrera <ignasi.barrera@abiquo.com>
2012-10-08 19:13:20 +0200

    Made install chef gems script portable
    Made install chef gems script portable to yum based
    distributions.
    
    This was tested with CentOS 5.6 with the apache2 recipe.
    Everything works fine except the startup of the apache
    server due to an issue in the apache2 recipe. When it creates
    the httpd.conf file it does no set the right permissions.

--- apis/chef/src/test/resources/one-recipe.sh
+++ apis/chef/src/test/resources/one-recipe.sh
@@ -1,6 +1,17 @@
 if [ ! -f /usr/bin/chef-client ]; then
-  apt-get update
-  apt-get install -y ruby ruby1.8-dev build-essential wget libruby-extras libruby1.8-extras
+  if which dpkg &> /dev/null; then
+    apt-get-update
+    apt-get install -y ruby ruby1.8-dev build-essential wget libruby-extras libruby1.8-extras
+  elif which rpm &> /dev/null; then
+    # Disable chef from the base repo (http://tickets.opscode.com/browse/CHEF-2906)
+    sed -i "s/\[base\]/\0\n\exclude=ruby*/g" /etc/yum.repos.d/CentOS-Base.repo
+    # Make sure to install an appropriate ruby version
+    yum erase -y ruby ruby-libs
+    rpm -Uvh http://rbel.co/rbel5
+    yum install -y ruby ruby-devel make gcc gcc-c++ kernel-devel automake autoconf wget
+  else
+    abort "we only support apt-get and yum right now... please contribute"
+  fi
   (
   mkdir -p /tmp/bootchef
   cd /tmp/bootchef
@@ -10,6 +21,10 @@ if [ ! -f /usr/bin/chef-client ]; then
   ruby setup.rb --no-format-executable
   rm -fr /tmp/bootchef
   )
+  if which rpm &> /dev/null; then
+    #Install gems provided by libruby-extras deb package (based on https://launchpad.net/ubuntu/precise/+package/libruby-extras)
+    /usr/bin/gem install cmdparse daemons log4r mmap ncurses --no-rdoc --no-ri --verbose
+  fi
   /usr/bin/gem install ohai chef --no-rdoc --no-ri --verbose
 fi
 mkdir -p /etc/chef
