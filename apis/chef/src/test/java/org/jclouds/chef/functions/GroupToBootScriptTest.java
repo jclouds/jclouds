f0fe60dbd2cb3485c034174115983cf51866069f
Ignasi Barrera <ignasi.barrera@abiquo.com>
2012-11-11 20:34:24 +0100

    Updated install gems test to the last version of the InstallChefGems statement

--- apis/chef/src/test/java/org/jclouds/chef/functions/GroupToBootScriptTest.java
+++ apis/chef/src/test/java/org/jclouds/chef/functions/GroupToBootScriptTest.java
@@ -112,7 +112,7 @@ public class GroupToBootScriptTest {
             fn.apply("foo").render(OsFamily.UNIX),
             Resources.toString(Resources.getResource("test_install_ruby." + ShellToken.SH.to(OsFamily.UNIX)),
                   Charsets.UTF_8)
-                  + "installChefGems || return 1\n"
+                  + "gem install ohai chef --no-rdoc --no-ri\n"
                   + Resources.toString(Resources.getResource("one-recipe.sh"), Charsets.UTF_8));
 
       verify(validatorKey);
