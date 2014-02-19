076e31b076d0b43fc80b0265fd3fa244fd549069
Andrew Gaul <gaul@apache.org>
2014-02-18 22:33:56 -0800

    Address Checkstyle violations

--- apis/ec2/src/main/java/org/jclouds/ec2/compute/EC2ComputeService.java
+++ apis/ec2/src/main/java/org/jclouds/ec2/compute/EC2ComputeService.java
@@ -54,8 +54,6 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.ImmutableMultimap.Builder;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 import com.google.common.util.concurrent.ListeningExecutorService;
