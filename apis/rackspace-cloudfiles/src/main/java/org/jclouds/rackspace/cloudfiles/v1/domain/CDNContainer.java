4073f7fdc4e11fc9ff1d419dc05731336cd9d8d0
Andrew Gaul <gaul@apache.org>
2014-08-08 11:29:10 -0700

    JCLOUDS-653: Address Guava 18 deprecations
    Mostly renaming Objects.toStringHelper to MoreObjects.toStringHelper.

--- apis/rackspace-cloudfiles/src/main/java/org/jclouds/rackspace/cloudfiles/v1/domain/CDNContainer.java
+++ apis/rackspace-cloudfiles/src/main/java/org/jclouds/rackspace/cloudfiles/v1/domain/CDNContainer.java
@@ -16,7 +16,7 @@
  */
 package org.jclouds.rackspace.cloudfiles.v1.domain;
 
-import static com.google.common.base.Objects.toStringHelper;
+import static com.google.common.base.MoreObjects.toStringHelper;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.beans.ConstructorProperties;
@@ -24,8 +24,8 @@ import java.net.URI;
 
 import javax.inject.Named;
 
+import com.google.common.base.MoreObjects.ToStringHelper;
 import com.google.common.base.Objects;
-import com.google.common.base.Objects.ToStringHelper;
 
 /**
  * Represents a CDN Container in Rackspace Cloud Files.
