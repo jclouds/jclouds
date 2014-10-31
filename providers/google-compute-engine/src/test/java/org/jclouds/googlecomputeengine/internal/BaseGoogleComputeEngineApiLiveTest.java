jclouds/jclouds-labs-google

initial commit to support GCE LB
add support targetPools, forwardingRules and httpHealthChecks API
add expectedTests and LiveTests for the above API

google-compute-engine/src/test/java/org/jclouds/googlecomputeengine/internal/BaseGoogleComputeEngineApiLiveTest.java
@@ -17,6 +17,7 @@
 package org.jclouds.googlecomputeengine.internal;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.jclouds.oauth.v2.OAuthTestUtils.setCredentialFromPemFile;
 import static org.jclouds.util.Predicates2.retry;
 import static org.testng.Assert.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
@@ -74,6 +75,13 @@ public class BaseGoogleComputeEngineApiLiveTest extends BaseApiLiveTest<GoogleCo
    public BaseGoogleComputeEngineApiLiveTest() {
       provider = "google-compute-engine";
    }
+
+    @Override
+    protected Properties setupProperties() {
+       Properties props = super.setupProperties();
+       setCredentialFromPemFile(props, provider + ".credential");
+       return props;
+    }
 
    protected GoogleComputeEngineApi create(Properties props, Iterable<Module> modules) {
       Injector injector = newBuilder().modules(modules).overrides(props).buildInjector();

jclouds/jclouds

providers/google-compute-engine/src/test/java/org/jclouds/googlecomputeengine/internal/BaseGoogleComputeEngineApiLiveTest.java
@@ -17,6 +17,7 @@
 package org.jclouds.googlecomputeengine.internal;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.jclouds.oauth.v2.OAuthTestUtils.setCredential;
 import static org.jclouds.util.Predicates2.retry;
 import static org.testng.Assert.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
@@ -70,10 +71,16 @@ public class BaseGoogleComputeEngineApiLiveTest extends BaseApiLiveTest<GoogleCo
    protected Predicate<AtomicReference<Operation>> regionOperationDonePredicate;
    protected Predicate<AtomicReference<Operation>> zoneOperationDonePredicate;
 
-
    public BaseGoogleComputeEngineApiLiveTest() {
       provider = "google-compute-engine";
    }
+
+    @Override
+    protected Properties setupProperties() {
+       Properties props = super.setupProperties();
+       setCredential(props, provider + ".credential");
+       return props;
+    }
 
    protected GoogleComputeEngineApi create(Properties props, Iterable<Module> modules) {
       Injector injector = newBuilder().modules(modules).overrides(props).buildInjector();