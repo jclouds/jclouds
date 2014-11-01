b04fd71998b4ad735b1da207bda924f1117e5d8e
Adrian Cole <adrian.f.cole@gmail.com>
2014-11-01 11:06:22 -0700

    JCLOUDS-750 FieldNamingStrategy is no longer required.

--- apis/docker/src/main/java/org/jclouds/docker/config/DockerParserModule.java
+++ apis/docker/src/main/java/org/jclouds/docker/config/DockerParserModule.java
@@ -16,35 +16,12 @@
  */
 package org.jclouds.docker.config;
 
-import java.lang.reflect.Field;
-
 import org.jclouds.json.config.GsonModule;
 
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.FieldNamingStrategy;
 import com.google.inject.AbstractModule;
 
 public class DockerParserModule extends AbstractModule {
    @Override protected void configure() {
-      bind(FieldNamingStrategy.class).toInstance(FIELD_NAMING_STRATEGY);
       bind(GsonModule.DateAdapter.class).to(GsonModule.Iso8601DateAdapter.class);
    }
-
-   /** When serializing, Most fields are UpperCamelCase, with some exceptions. */
-   private static final FieldNamingStrategy FIELD_NAMING_STRATEGY = new FieldNamingStrategy() {
-      private final FieldNamingStrategy delegate = FieldNamingPolicy.UPPER_CAMEL_CASE;
-
-      @Override public String translateName(Field f) {
-         String result = delegate.translateName(f);
-         // IP not Ip as code wins over docs https://github.com/docker/docker/blob/master/daemon/network_settings.go
-         if (result.equals("IpAddress")) {
-            return "IPAddress";
-         } else if (result.equals("IpPrefixLen")) {
-            return "IPPrefixLen";
-         } else if (result.equals("Ip")) {
-            return "IP";
-         }
-         return result;
-      }
-   };
 }
