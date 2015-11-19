jclouds/jclouds

commit 79fe91bd035461d9344c03bda276cb12a7a10b1a

JCLOUDS-979: Fix range get for LocalBlobStore

blobstore/src/main/java/org/jclouds/blobstore/config/LocalBlobStore.java
@@ -80,9 +80,12 @@ import org.jclouds.http.HttpRequest;
 import org.jclouds.http.HttpResponse;
 import org.jclouds.http.HttpResponseException;
 import org.jclouds.http.HttpUtils;
+import org.jclouds.io.ByteStreams2;
 import org.jclouds.io.ContentMetadata;
 import org.jclouds.io.ContentMetadataCodec;
 import org.jclouds.io.Payload;
+import org.jclouds.io.payloads.ByteArrayPayload;
+import org.jclouds.io.payloads.FilePayload;
 import org.jclouds.logging.Logger;
 import org.jclouds.util.Closeables2;
 
@@ -684,9 +687,18 @@ public final class LocalBlobStore implements BlobStore {
                if (last + 1 > blob.getPayload().getContentMetadata().getContentLength()) {
                   last = blob.getPayload().getContentMetadata().getContentLength() - 1;
                }
-               // We must call getRawContent to work around Blob.setPayload calling ByteSourcePayload.release.  Local
-               // blobstores always return either a ByteArrayPayload or FilePayload.
-               ByteSource byteSource = (ByteSource) blob.getPayload().getRawContent();
+               // We must call getRawContent to work around Blob.setPayload calling ByteSourcePayload.release. If the
+               // Local blobstore returns a ByteArrayPayload or FilePayload it uses a stream, otherwise it uses a
+               // byte array.
+               ByteSource byteSource;
+               try {
+                  Object inputStream = blob.getPayload().getRawContent();
+                  byteSource = (inputStream instanceof ByteArrayPayload || inputStream instanceof FilePayload) ?
+                        (ByteSource) blob.getPayload().getRawContent()
+                        : ByteSource.wrap(ByteStreams2.toByteArrayAndClose(blob.getPayload().openStream()));
+               } catch (IOException e) {
+                  throw new RuntimeException(e);
+               }
                streams.add(byteSource.slice(offset, last - offset + 1));
                size += last - offset + 1;
                blob.getAllHeaders().put(HttpHeaders.CONTENT_RANGE,

commit 8d87bfc61bd5190cd0711f6be0189f70741a8c61

Avoid buffering ByteSource when not needed
This allows range requests of large files to work.  Regression from
79fe91bd035461d9344c03bda276cb12a7a10b1a.

blobstore/src/main/java/org/jclouds/blobstore/config/LocalBlobStore.java
@@ -84,8 +84,6 @@ import org.jclouds.io.ByteStreams2;
 import org.jclouds.io.ContentMetadata;
 import org.jclouds.io.ContentMetadataCodec;
 import org.jclouds.io.Payload;
-import org.jclouds.io.payloads.ByteArrayPayload;
-import org.jclouds.io.payloads.FilePayload;
 import org.jclouds.logging.Logger;
 import org.jclouds.util.Closeables2;
 
@@ -662,17 +660,16 @@ public final class LocalBlobStore implements BlobStore {
             long size = 0;
             ImmutableList.Builder<ByteSource> streams = ImmutableList.builder();
 
-            // We must call getRawContent to work around Blob.setPayload calling ByteSourcePayload.release. If the
-            // Local blobstore returns a ByteArrayPayload or FilePayload it uses a stream, otherwise it uses a
-            // byte array.
+            // Try to convert payload to ByteSource, otherwise wrap it.
             ByteSource byteSource;
             try {
-               Object inputStream = blob.getPayload().getRawContent();
-               byteSource = (inputStream instanceof ByteArrayPayload || inputStream instanceof FilePayload) ?
-                     (ByteSource) blob.getPayload().getRawContent()
-                     : ByteSource.wrap(ByteStreams2.toByteArrayAndClose(blob.getPayload().openStream()));
-            } catch (IOException e) {
-               throw new RuntimeException(e);
+               byteSource = (ByteSource) blob.getPayload().getRawContent();
+            } catch (ClassCastException cce) {
+               try {
+                  byteSource = ByteSource.wrap(ByteStreams2.toByteArrayAndClose(blob.getPayload().openStream()));
+               } catch (IOException e) {
+                  throw new RuntimeException(e);
+               }
             }
 
             for (String s : options.getRanges()) {