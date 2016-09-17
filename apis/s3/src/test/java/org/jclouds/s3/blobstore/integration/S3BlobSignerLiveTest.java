jclouds/jclouds

commit 41bdd9c5a9e02aa407e7036d3fddceb138f8b3b7

Skip unimplemented signed URL tests in S3 provider
jclouds aws-s3 provider supports these but s3 does not.  Also expect
failure in atmos and azureblob.  Follow on to
a4c40e15cdddda30a9683d12bf022d94025567c8.

apis/s3/src/test/java/org/jclouds/s3/blobstore/integration/S3BlobSignerLiveTest.java
@@ -16,8 +16,13 @@
  */
 package org.jclouds.s3.blobstore.integration;
 
+import static org.testng.Assert.fail;
+
+import java.io.IOException;
+
 import org.jclouds.blobstore.integration.internal.BaseBlobSignerLiveTest;
 import org.jclouds.blobstore.integration.internal.BaseBlobStoreIntegrationTest;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 @Test(groups = "live", testName = "S3BlobSignerLiveTest")
@@ -26,5 +31,45 @@ public class S3BlobSignerLiveTest extends BaseBlobSignerLiveTest {
    public S3BlobSignerLiveTest() {
       provider = "s3";
       BaseBlobStoreIntegrationTest.SANITY_CHECK_RETURNED_BUCKET_NAME = true;
+   }
+
+   @Test
+   public void testSignGetUrlWithTime() throws InterruptedException, IOException {
+      try {
+         super.testSignGetUrlWithTime();
+         fail();
+      } catch (UnsupportedOperationException uoe) {
+         throw new SkipException("not supported by S3 signer", uoe);
+      }
+   }
+
+   @Test
+   public void testSignGetUrlWithTimeExpired() throws InterruptedException, IOException {
+      try {
+         super.testSignGetUrlWithTimeExpired();
+         fail();
+      } catch (UnsupportedOperationException uoe) {
+         throw new SkipException("not supported by S3 signer", uoe);
+      }
+   }
+
+   @Test
+   public void testSignPutUrlWithTime() throws Exception {
+      try {
+         super.testSignPutUrlWithTime();
+         fail();
+      } catch (UnsupportedOperationException uoe) {
+         throw new SkipException("not supported by S3 signer", uoe);
+      }
+   }
+
+   @Test
+   public void testSignPutUrlWithTimeExpired() throws Exception {
+      try {
+         super.testSignPutUrlWithTimeExpired();
+         fail();
+      } catch (UnsupportedOperationException uoe) {
+         throw new SkipException("not supported by S3 signer", uoe);
+      }
    }
 }

apis/atmos/src/test/java/org/jclouds/atmos/blobstore/integration/AtmosBlobSignerLiveTest.java
@@ -16,6 +16,8 @@
  */
 package org.jclouds.atmos.blobstore.integration;
 
+import static org.testng.Assert.fail;
+
 import java.io.IOException;
 
 import org.jclouds.blobstore.integration.internal.BaseBlobSignerLiveTest;
@@ -32,6 +34,7 @@ public class AtmosBlobSignerLiveTest extends BaseBlobSignerLiveTest {
    public void testSignGetUrlWithTime() throws InterruptedException, IOException {
       try {
          super.testSignGetUrlWithTime();
+         fail();
       } catch (UnsupportedOperationException uoe) {
          throw new SkipException("not supported in Atmos", uoe);
       }
@@ -41,6 +44,7 @@ public class AtmosBlobSignerLiveTest extends BaseBlobSignerLiveTest {
    public void testSignGetUrlWithTimeExpired() throws InterruptedException, IOException {
       try {
          super.testSignGetUrlWithTimeExpired();
+         fail();
       } catch (UnsupportedOperationException uoe) {
          throw new SkipException("not supported in Atmos", uoe);
       }
@@ -50,6 +54,7 @@ public class AtmosBlobSignerLiveTest extends BaseBlobSignerLiveTest {
    public void testSignPutUrlWithTime() throws Exception {
       try {
          super.testSignPutUrlWithTime();
+         fail();
       } catch (UnsupportedOperationException uoe) {
          throw new SkipException("not supported in Atmos", uoe);
       }
@@ -59,6 +64,7 @@ public class AtmosBlobSignerLiveTest extends BaseBlobSignerLiveTest {
    public void testSignPutUrlWithTimeExpired() throws Exception {
       try {
          super.testSignPutUrlWithTimeExpired();
+         fail();
       } catch (UnsupportedOperationException uoe) {
          throw new SkipException("not supported in Atmos", uoe);
       }

providers/azureblob/src/test/java/org/jclouds/azureblob/blobstore/integration/AzureBlobSignerLiveTest.java
@@ -16,6 +16,8 @@
  */
 package org.jclouds.azureblob.blobstore.integration;
 
+import static org.testng.Assert.fail;
+
 import java.io.IOException;
 
 import org.jclouds.blobstore.integration.internal.BaseBlobSignerLiveTest;
@@ -32,6 +34,7 @@ public class AzureBlobSignerLiveTest extends BaseBlobSignerLiveTest {
    public void testSignGetUrlWithTime() throws InterruptedException, IOException {
       try {
          super.testSignGetUrlWithTime();
+         fail();
       } catch (UnsupportedOperationException uoe) {
          throw new SkipException("not supported in Azure", uoe);
       }
@@ -41,6 +44,7 @@ public class AzureBlobSignerLiveTest extends BaseBlobSignerLiveTest {
    public void testSignGetUrlWithTimeExpired() throws InterruptedException, IOException {
       try {
          super.testSignGetUrlWithTimeExpired();
+         fail();
       } catch (UnsupportedOperationException uoe) {
          throw new SkipException("not supported in Azure", uoe);
       }
@@ -50,6 +54,7 @@ public class AzureBlobSignerLiveTest extends BaseBlobSignerLiveTest {
    public void testSignPutUrlWithTime() throws Exception {
       try {
          super.testSignPutUrlWithTime();
+         fail();
       } catch (UnsupportedOperationException uoe) {
          throw new SkipException("not supported in Azure", uoe);
       }
@@ -59,6 +64,7 @@ public class AzureBlobSignerLiveTest extends BaseBlobSignerLiveTest {
    public void testSignPutUrlWithTimeExpired() throws Exception {
       try {
          super.testSignPutUrlWithTimeExpired();
+         fail();
       } catch (UnsupportedOperationException uoe) {
          throw new SkipException("not supported in Azure", uoe);
       }

commit 76995a363d1cabaee26066b52a476ae28c4fd638

Do not fail AWS S3 signer tests with expiry
Regression from 41bdd9c5a9e02aa407e7036d3fddceb138f8b3b7.
References #1008.

apis/s3/src/test/java/org/jclouds/s3/blobstore/integration/S3BlobSignerLiveTest.java
@@ -33,11 +33,17 @@ public class S3BlobSignerLiveTest extends BaseBlobSignerLiveTest {
       BaseBlobStoreIntegrationTest.SANITY_CHECK_RETURNED_BUCKET_NAME = true;
    }
 
+   protected boolean supportsUrlWithTime() {
+      return false;
+   }
+
    @Test
    public void testSignGetUrlWithTime() throws InterruptedException, IOException {
       try {
          super.testSignGetUrlWithTime();
-         fail();
+         if (!supportsUrlWithTime()) {
+            fail();
+         }
       } catch (UnsupportedOperationException uoe) {
          throw new SkipException("not supported by S3 signer", uoe);
       }
@@ -47,7 +53,9 @@ public class S3BlobSignerLiveTest extends BaseBlobSignerLiveTest {
    public void testSignGetUrlWithTimeExpired() throws InterruptedException, IOException {
       try {
          super.testSignGetUrlWithTimeExpired();
-         fail();
+         if (!supportsUrlWithTime()) {
+            fail();
+         }
       } catch (UnsupportedOperationException uoe) {
          throw new SkipException("not supported by S3 signer", uoe);
       }
@@ -57,7 +65,9 @@ public class S3BlobSignerLiveTest extends BaseBlobSignerLiveTest {
    public void testSignPutUrlWithTime() throws Exception {
       try {
          super.testSignPutUrlWithTime();
-         fail();
+         if (!supportsUrlWithTime()) {
+            fail();
+         }
       } catch (UnsupportedOperationException uoe) {
          throw new SkipException("not supported by S3 signer", uoe);
       }
@@ -67,7 +77,9 @@ public class S3BlobSignerLiveTest extends BaseBlobSignerLiveTest {
    public void testSignPutUrlWithTimeExpired() throws Exception {
       try {
          super.testSignPutUrlWithTimeExpired();
-         fail();
+         if (!supportsUrlWithTime()) {
+            fail();
+         }
       } catch (UnsupportedOperationException uoe) {
          throw new SkipException("not supported by S3 signer", uoe);
       }

providers/aws-s3/src/test/java/org/jclouds/aws/s3/blobstore/integration/AWSS3BlobSignerLiveTest.java
@@ -16,9 +16,11 @@
  */
 package org.jclouds.aws.s3.blobstore.integration;
 
+import java.io.IOException;
 import java.util.Properties;
 
 import org.jclouds.Constants;
+import org.jclouds.blobstore.integration.internal.BaseBlobSignerLiveTest;
 import org.jclouds.s3.blobstore.integration.S3BlobSignerLiveTest;
 import org.testng.annotations.Test;
 
@@ -34,5 +36,10 @@ public class AWSS3BlobSignerLiveTest extends S3BlobSignerLiveTest {
       overrides.setProperty(Constants.PROPERTY_STRIP_EXPECT_HEADER, "true");
       overrides.setProperty(Constants.PROPERTY_SESSION_INTERVAL, "1");
       return overrides;
+   }
+
+   @Override
+   protected boolean supportsUrlWithTime() {
+      return true;
    }
 }