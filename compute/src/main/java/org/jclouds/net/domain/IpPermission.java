b8fd47ba8d0086f3b1b15f43360a1f4c4dd9eff0
Geoff Macartney <geoff.macartney@cloudsoftcorp.com>
2017-02-14 16:10:52 +0000

    JCLOUDS-1237: Add compareTo() for IpPermission.

--- compute/src/main/java/org/jclouds/net/domain/IpPermission.java
+++ compute/src/main/java/org/jclouds/net/domain/IpPermission.java
@@ -22,7 +22,12 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.collect.Iterables.transform;
 import static org.jclouds.util.Strings2.isCidrFormat;
 
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.Set;
+
+import org.jclouds.net.util.IpPermissions;
 
 import com.google.common.annotations.Beta;
 import com.google.common.base.Function;
@@ -39,7 +44,7 @@ import com.google.common.collect.Sets;
  * Ingress access to a destination protocol on particular ports by source, which could be an ip
  * range (cidrblock), set of explicit security group ids in the current tenant, or security group
  * names in another tenant.
- * 
+ *
  * @see IpPermissions
  */
 @Beta
@@ -58,7 +63,26 @@ public class IpPermission implements Comparable<IpPermission> {
       private Set<String> exclusionCidrBlocks = Sets.newLinkedHashSet();
 
       /**
-       * 
+       * Creates a builder initialized from an existing permission.
+       * @param permission The existing permission.
+       * @return the builder.
+       */
+      public Builder fromPermission(IpPermission permission) {
+         this.ipProtocol = permission.ipProtocol;
+         this.fromPort = permission.fromPort;
+         this.toPort = permission.toPort;
+         this.tenantIdGroupNamePairs = LinkedHashMultimap.create();
+         tenantIdGroupNamePairs.putAll(permission.tenantIdGroupNamePairs);
+         this.groupIds = Sets.newLinkedHashSet();
+         this.groupIds.addAll(permission.groupIds);
+         this.cidrBlocks = Sets.newLinkedHashSet();
+         this.cidrBlocks.addAll(permission.cidrBlocks);
+         this.exclusionCidrBlocks = Sets.newLinkedHashSet();
+         this.exclusionCidrBlocks.addAll(permission.exclusionCidrBlocks);
+         return this;
+      }
+
+      /**
        * @see IpPermission#getIpProtocol()
        */
       public Builder ipProtocol(IpProtocol ipProtocol) {
@@ -67,7 +91,6 @@ public class IpPermission implements Comparable<IpPermission> {
       }
 
       /**
-       * 
        * @see IpPermission#getFromPort()
        */
       public Builder fromPort(int fromPort) {
@@ -76,7 +99,6 @@ public class IpPermission implements Comparable<IpPermission> {
       }
 
       /**
-       * 
        * @see IpPermission#getToPort()
        */
       public Builder toPort(int toPort) {
@@ -129,7 +151,7 @@ public class IpPermission implements Comparable<IpPermission> {
       @Beta
       public Builder exclusionCidrBlock(String exclusionCidrBlock) {
          checkArgument(isCidrFormat(exclusionCidrBlock), "exclusionCidrBlock %s is not a valid CIDR",
-               exclusionCidrBlock);
+            exclusionCidrBlock);
          this.exclusionCidrBlocks.add(exclusionCidrBlock);
          return this;
       }
@@ -167,7 +189,7 @@ public class IpPermission implements Comparable<IpPermission> {
 
       public IpPermission build() {
          return new IpPermission(ipProtocol, fromPort, toPort, tenantIdGroupNamePairs, groupIds, cidrBlocks,
-               exclusionCidrBlocks);
+            exclusionCidrBlocks);
       }
    }
 
@@ -180,25 +202,18 @@ public class IpPermission implements Comparable<IpPermission> {
    private final Set<String> exclusionCidrBlocks;
 
    public IpPermission(IpProtocol ipProtocol, int fromPort, int toPort,
-         Multimap<String, String> tenantIdGroupNamePairs, Iterable<String> groupIds, Iterable<String> cidrBlocks,
-         Iterable<String> exclusionCidrBlocks) {
+                       Multimap<String, String> tenantIdGroupNamePairs, Iterable<String> groupIds, Iterable<String> cidrBlocks,
+                       Iterable<String> exclusionCidrBlocks) {
       this.fromPort = fromPort;
       this.toPort = toPort;
       this.tenantIdGroupNamePairs = ImmutableMultimap.copyOf(checkNotNull(tenantIdGroupNamePairs,
-            "tenantIdGroupNamePairs"));
+         "tenantIdGroupNamePairs"));
       this.ipProtocol = checkNotNull(ipProtocol, "ipProtocol");
       this.groupIds = ImmutableSet.copyOf(checkNotNull(groupIds, "groupIds"));
       this.cidrBlocks = ImmutableSet.copyOf(checkNotNull(cidrBlocks, "cidrBlocks"));
       this.exclusionCidrBlocks = ImmutableSet.copyOf(checkNotNull(exclusionCidrBlocks, "exclusionCidrBlocks"));
    }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public int compareTo(IpPermission o) {
-      return (this == o) ? 0 : getIpProtocol().compareTo(o.getIpProtocol());
-   }
 
    /**
     * destination IP protocol
@@ -252,6 +267,41 @@ public class IpPermission implements Comparable<IpPermission> {
       return exclusionCidrBlocks;
    }
 
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   public int compareTo(IpPermission that) {
+      if (this == that) return 0;
+      final int proto = getIpProtocol().compareTo(that.getIpProtocol());
+      if (proto != 0) return proto;
+
+      final int fromP = Integer.valueOf(this.fromPort).compareTo(Integer.valueOf(that.fromPort));
+      if (fromP != 0) return fromP;
+
+      final int toP = Integer.valueOf(this.toPort).compareTo(Integer.valueOf(that.toPort));
+      if (toP != 0) return toP;
+
+      final int tenantGroups = new LinkedMultiMapComparator<String, String>()
+         .compare(this.tenantIdGroupNamePairs, that.tenantIdGroupNamePairs);
+      if (tenantGroups != 0) return tenantGroups;
+
+      final int groupIdComp = new CollectionComparator<String>()
+         .compare(this.groupIds, that.groupIds);
+      if (groupIdComp != 0) return groupIdComp;
+
+      final int cidrComp = new CollectionComparator<String>()
+         .compare(this.cidrBlocks, that.cidrBlocks);
+      if (cidrComp != 0) return cidrComp;
+
+      final int exclusionsComp = new CollectionComparator<String>()
+         .compare(this.exclusionCidrBlocks, that.exclusionCidrBlocks);
+      if (exclusionsComp != 0) return exclusionsComp;
+
+      return 0;
+   }
+
    @Override
    public boolean equals(Object o) {
       if (this == o)
@@ -261,15 +311,15 @@ public class IpPermission implements Comparable<IpPermission> {
          return false;
       IpPermission that = IpPermission.class.cast(o);
       return equal(this.ipProtocol, that.ipProtocol) && equal(this.fromPort, that.fromPort)
-            && equal(this.toPort, that.toPort) && equal(this.tenantIdGroupNamePairs, that.tenantIdGroupNamePairs)
-            && equal(this.groupIds, that.groupIds) && equal(this.cidrBlocks, that.cidrBlocks)
-            && equal(this.exclusionCidrBlocks, that.exclusionCidrBlocks);
+         && equal(this.toPort, that.toPort) && equal(this.tenantIdGroupNamePairs, that.tenantIdGroupNamePairs)
+         && equal(this.groupIds, that.groupIds) && equal(this.cidrBlocks, that.cidrBlocks)
+         && equal(this.exclusionCidrBlocks, that.exclusionCidrBlocks);
    }
 
    @Override
    public int hashCode() {
       return Objects.hashCode(ipProtocol, fromPort, toPort, tenantIdGroupNamePairs, groupIds, cidrBlocks,
-            exclusionCidrBlocks);
+         exclusionCidrBlocks);
    }
 
    @Override
@@ -279,8 +329,78 @@ public class IpPermission implements Comparable<IpPermission> {
 
    protected ToStringHelper string() {
       return Objects.toStringHelper("").add("ipProtocol", ipProtocol).add("fromPort", fromPort)
-            .add("toPort", toPort).add("tenantIdGroupNamePairs", tenantIdGroupNamePairs).add("groupIds", groupIds)
-            .add("cidrBlocks", cidrBlocks).add("exclusionCidrBlocks", exclusionCidrBlocks);
+         .add("toPort", toPort).add("tenantIdGroupNamePairs", tenantIdGroupNamePairs).add("groupIds", groupIds)
+         .add("cidrBlocks", cidrBlocks).add("exclusionCidrBlocks", exclusionCidrBlocks);
+   }
+
+
+   // A private tool for use in implementing a consistent compareTo relation.
+   private static class LinkedMultiMapComparator<K extends Comparable, V> implements Comparator<Multimap<K, V>> {
+
+      /**
+       * Compares {@link Multimap}s, in order of iterators.
+       * If two keys do not compare as zero, the key comparison result is used as the comparison result.
+       * For keys that are equal, the value collections are compared with {@link CollectionComparator}.
+       * If all entries compare as zero the map sizes determine the result.
+       *
+       * @param map1 The first map for comparison
+       * @param map2 The second map for comparison
+       * @return the comparison relation value
+       */
+      @Override
+      public int compare(Multimap<K, V> map1, Multimap<K, V> map2) {
+         final Iterator<K> leftIter = map1.keySet().iterator();
+         final Iterator<K> rightIter = map2.keySet().iterator();
+         while (leftIter.hasNext() && rightIter.hasNext()) {
+            K key1 = leftIter.next();
+            K key2 = rightIter.next();
+
+            int keyComp = key1.compareTo(key2);
+            if (keyComp != 0) return keyComp;
+
+            final int valuesComp = new CollectionComparator().compare(map1.get(key1), map2.get(key2));
+            if (valuesComp != 0) return valuesComp;
+         }
+         if (!leftIter.hasNext() && rightIter.hasNext()) {
+            return -1;
+         }
+         if (leftIter.hasNext() && !rightIter.hasNext()) {
+            return +1;
+         }
+         return 0;
+      }
+   }
+
+   // A private tool for use in implementing a consistent compareTo relation.
+   private static class CollectionComparator<T extends Comparable> implements Comparator<Collection<T>> {
+
+      /**
+       * Compares collections of comparable objects, in order of iterator.
+       * Iterates through the collections in step.
+       * If two entries do not compare as zero, the comparison result is the result of this method.
+       * If all entries compare as zero, then the collection sizes determine the result.
+       *
+       * @param o1 The first collection to compare.
+       * @param o2 The second collection to compare.
+       * @return The comparison relation value.
+       */
+      @Override
+      public int compare(Collection<T> o1, Collection<T> o2) {
+
+         final Iterator<T> leftIter = o1.iterator();
+         final Iterator<T> rightIter = o2.iterator();
+         while (leftIter.hasNext() && rightIter.hasNext()) {
+            int comp = leftIter.next().compareTo(rightIter.next());
+            if (comp != 0) return comp;
+         }
+         if (!leftIter.hasNext() && rightIter.hasNext()) {
+            return -1;
+         }
+         if (leftIter.hasNext() && !rightIter.hasNext()) {
+            return +1;
+         }
+         return 0;
+      }
    }
 
 }
