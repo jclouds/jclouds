73875884b58e93a88676ecc20b174db17efd13cd
Michelle Zhang <q225zhan@uwaterloo.ca>
2017-03-28 23:33:07 -0400

    JCLOUDS-1217/JCLOUDS-1258: Add Canada, London and China regions to AWS

--- apis/sts/src/main/java/org/jclouds/aws/domain/Region.java
+++ apis/sts/src/main/java/org/jclouds/aws/domain/Region.java
@@ -47,6 +47,11 @@ public class Region {
    public static final String EU_WEST_1 = "eu-west-1";
 
    /**
+    * EU (London)
+    */
+   public static final String EU_WEST_2 = "eu-west-2";
+
+   /**
     * EU (Frankfurt)
     */
    public static final String EU_CENTRAL_1 = "eu-central-1";
@@ -82,12 +87,17 @@ public class Region {
     * and DELETES.
     */
    public static final String US_WEST_1 = "us-west-1";
-  
+
    /**
     * US-West-2 (Oregon)
     */
    public static final String US_WEST_2 = "us-west-2";
-   
+
+   /**
+    * CA-CENTRAL-1 (Canada)
+    */
+   public static final String CA_CENTRAL_1 = "ca-central-1";
+
    /**
     * SA-EAST-1 (Sao Paolo)
     * 
@@ -115,24 +125,29 @@ public class Region {
     * Region in Tokyo, launched March 2, 2011. This region improves latency for Asia-based users
     */
    public static final String AP_NORTHEAST_1 = "ap-northeast-1";
-   
+
    /**
     * Region in Seoul, launched January 6, 2016. This region improves latency for Asia-based users
     */
    public static final String AP_NORTHEAST_2 = "ap-northeast-2";
 
-   public static final Set<String> DEFAULT_S3 = ImmutableSet.of(US_STANDARD, US_WEST_1, US_WEST_2, EU_WEST_1, EU_CENTRAL_1, SA_EAST_1,
-         AP_SOUTHEAST_1, AP_SOUTHEAST_2, AP_SOUTH_1, AP_NORTHEAST_1, AP_NORTHEAST_2);
+   /**
+    * Region in Beijing, launched early 2014. This region improves latency for China users
+    */
+    public static final String CN_NORTH_1 = "cn-north-1";
 
-   public static final Set<String> DEFAULT_REGIONS = ImmutableSet.of(US_EAST_1, US_WEST_1, US_WEST_2, SA_EAST_1,
-         EU_WEST_1, EU_CENTRAL_1, AP_SOUTHEAST_1, AP_SOUTHEAST_2, AP_SOUTH_1, AP_NORTHEAST_1, AP_NORTHEAST_2);
+   public static final Set<String> DEFAULT_S3 = ImmutableSet.of(US_STANDARD, US_WEST_1, US_WEST_2, CA_CENTRAL_1, EU_WEST_1, EU_WEST_2, EU_CENTRAL_1, SA_EAST_1,
+         AP_SOUTHEAST_1, AP_SOUTHEAST_2, AP_SOUTH_1, AP_NORTHEAST_1, AP_NORTHEAST_2, CN_NORTH_1);
+
+   public static final Set<String> DEFAULT_REGIONS = ImmutableSet.of(US_EAST_1, US_WEST_1, US_WEST_2, CA_CENTRAL_1, SA_EAST_1,
+         EU_WEST_1, EU_WEST_2, EU_CENTRAL_1, AP_SOUTHEAST_1, AP_SOUTHEAST_2, AP_SOUTH_1, AP_NORTHEAST_1, AP_NORTHEAST_2, CN_NORTH_1);
 
    public static Properties regionPropertiesS3() {
 
       Properties properties = regionProperties();
       properties.setProperty(PROPERTY_REGIONS, Joiner.on(',').join(DEFAULT_S3));
       // note that due to US_STANDARD the codes include US instead of US-VA
-      properties.setProperty(PROPERTY_ISO3166_CODES, "US,US-CA,US-OR,BR-SP,IE,DE-HE,SG,AU-NSW,IN-MH,JP-13,KR-11");
+      properties.setProperty(PROPERTY_ISO3166_CODES, "US,US-CA,US-OR,CA,BR-SP,IE,GB-LND,DE-HE,SG,AU-NSW,IN-MH,JP-13,KR-11,CN-11");
       properties.setProperty(PROPERTY_REGION + "." + US_STANDARD + "." + ISO3166_CODES, "US");
       return properties;
    }
@@ -140,18 +155,21 @@ public class Region {
    public static Properties regionProperties() {
       Properties properties = new Properties();
       properties.setProperty(PROPERTY_REGIONS, Joiner.on(',').join(DEFAULT_REGIONS));
-      properties.setProperty(PROPERTY_ISO3166_CODES, "US-VA,US-CA,US-OR,BR-SP,IE,DE-HE,SG,AU-NSW,IN-MH,JP-13,KR-11");
+      properties.setProperty(PROPERTY_ISO3166_CODES, "US-VA,US-CA,US-OR,CA,BR-SP,IE,GB-LND,DE-HE,SG,AU-NSW,IN-MH,JP-13,KR-11,CN-11");
       properties.setProperty(PROPERTY_REGION + "." + US_EAST_1 + "." + ISO3166_CODES, "US-VA");
       properties.setProperty(PROPERTY_REGION + "." + US_WEST_1 + "." + ISO3166_CODES, "US-CA");
       properties.setProperty(PROPERTY_REGION + "." + US_WEST_2 + "." + ISO3166_CODES, "US-OR");
+      properties.setProperty(PROPERTY_REGION + "." + CA_CENTRAL_1 + "." + ISO3166_CODES, "CA");
       properties.setProperty(PROPERTY_REGION + "." + SA_EAST_1 + "." + ISO3166_CODES, "BR-SP");
       properties.setProperty(PROPERTY_REGION + "." + EU_WEST_1 + "." + ISO3166_CODES, "IE");
+      properties.setProperty(PROPERTY_REGION + "." + EU_WEST_2 + "." + ISO3166_CODES, "GB-LND");
       properties.setProperty(PROPERTY_REGION + "." + EU_CENTRAL_1 + "." + ISO3166_CODES, "DE-HE");
       properties.setProperty(PROPERTY_REGION + "." + AP_SOUTHEAST_1 + "." + ISO3166_CODES, "SG");
       properties.setProperty(PROPERTY_REGION + "." + AP_SOUTHEAST_2 + "." + ISO3166_CODES, "AU-NSW");
       properties.setProperty(PROPERTY_REGION + "." + AP_SOUTH_1 + "." + ISO3166_CODES, "IN-MH");
       properties.setProperty(PROPERTY_REGION + "." + AP_NORTHEAST_1 + "." + ISO3166_CODES, "JP-13");
       properties.setProperty(PROPERTY_REGION + "." + AP_NORTHEAST_2 + "." + ISO3166_CODES, "KR-11");
+      properties.setProperty(PROPERTY_REGION + "." + CN_NORTH_1 + "." + ISO3166_CODES, "CN-11");
       return properties;
    }
 }
