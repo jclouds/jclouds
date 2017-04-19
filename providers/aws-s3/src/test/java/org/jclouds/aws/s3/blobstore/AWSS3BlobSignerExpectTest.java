jclouds/jclouds

commit 7a110b31ba7a7bd68c9c180a922391a46ff5510e

JCLOUDS-255: Support S3 signed URLs with expiry
Mostly code motion from AWSS3BlobRequestSigner to S3BlobRequestSigner
with some additional cleanups.

apis/s3/src/main/java/org/jclouds/s3/blobstore/S3BlobRequestSigner.java
@@ -33,6 +33,7 @@ import org.jclouds.rest.internal.RestAnnotationProcessor;
 import org.jclouds.s3.S3Client;
 import org.jclouds.s3.blobstore.functions.BlobToObject;
 import org.jclouds.s3.domain.S3Object;
+import org.jclouds.s3.filters.RequestAuthorizeSignature;
 import org.jclouds.s3.options.PutObjectOptions;
 
 import com.google.common.collect.ImmutableList;
@@ -40,6 +41,8 @@ import com.google.common.reflect.Invokable;
 
 @Singleton
 public class S3BlobRequestSigner<T extends S3Client> implements BlobRequestSigner {
+   private final RequestAuthorizeSignature authSigner;
+
    protected final RestAnnotationProcessor processor;
    protected final BlobToObject blobToObject;
    protected final BlobToHttpGetOptions blob2HttpGetOptions;
@@ -50,14 +53,16 @@ public class S3BlobRequestSigner<T extends S3Client> implements BlobRequestSigne
 
    @Inject
    public S3BlobRequestSigner(RestAnnotationProcessor processor, BlobToObject blobToObject,
-         BlobToHttpGetOptions blob2HttpGetOptions, Class<T> interfaceClass) throws SecurityException,
-         NoSuchMethodException {
+         BlobToHttpGetOptions blob2HttpGetOptions, Class<T> interfaceClass,
+         RequestAuthorizeSignature authSigner)
+         throws SecurityException, NoSuchMethodException {
       this.processor = checkNotNull(processor, "processor");
       this.blobToObject = checkNotNull(blobToObject, "blobToObject");
       this.blob2HttpGetOptions = checkNotNull(blob2HttpGetOptions, "blob2HttpGetOptions");
       this.getMethod = method(interfaceClass, "getObject", String.class, String.class, GetOptions[].class);
       this.deleteMethod = method(interfaceClass, "deleteObject", String.class, String.class);
       this.createMethod = method(interfaceClass, "putObject", String.class, S3Object.class, PutObjectOptions[].class);
+      this.authSigner = authSigner;
    }
 
    @Override
@@ -69,7 +74,10 @@ public class S3BlobRequestSigner<T extends S3Client> implements BlobRequestSigne
 
    @Override
    public HttpRequest signGetBlob(String container, String name, long timeInSeconds) {
-      throw new UnsupportedOperationException();
+      checkNotNull(container, "container");
+      checkNotNull(name, "name");
+      HttpRequest request = processor.apply(Invocation.create(getMethod, ImmutableList.<Object> of(container, name)));
+      return cleanRequest(authSigner.signForTemporaryAccess(request, timeInSeconds));
    }
 
    @Override
@@ -82,7 +90,11 @@ public class S3BlobRequestSigner<T extends S3Client> implements BlobRequestSigne
 
    @Override
    public HttpRequest signPutBlob(String container, Blob blob, long timeInSeconds) {
-      throw new UnsupportedOperationException();
+      checkNotNull(container, "container");
+      checkNotNull(blob, "blob");
+      HttpRequest request = processor.apply(Invocation.create(createMethod,
+         ImmutableList.<Object>of(container, blobToObject.apply(blob))));
+      return cleanRequest(authSigner.signForTemporaryAccess(request, timeInSeconds));
    }
 
    @Override

apis/s3/src/main/java/org/jclouds/s3/filters/RequestAuthorizeSignatureV2.java
@@ -29,9 +29,11 @@ import static org.jclouds.s3.reference.S3Constants.PROPERTY_S3_VIRTUAL_HOST_BUCK
 import static org.jclouds.util.Strings2.toInputStream;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Resource;
 import javax.inject.Inject;
@@ -42,14 +44,17 @@ import javax.inject.Singleton;
 import org.jclouds.Constants;
 import org.jclouds.aws.domain.SessionCredentials;
 import org.jclouds.crypto.Crypto;
+import org.jclouds.date.DateService;
 import org.jclouds.date.TimeStamp;
 import org.jclouds.domain.Credentials;
 import org.jclouds.http.HttpException;
 import org.jclouds.http.HttpRequest;
+import org.jclouds.http.HttpRequestFilter;
 import org.jclouds.http.HttpUtils;
 import org.jclouds.http.internal.SignatureWire;
 import org.jclouds.logging.Logger;
 import org.jclouds.rest.RequestSigner;
+import org.jclouds.s3.reference.S3Constants;
 import org.jclouds.s3.util.S3Utils;
 
 import com.google.common.annotations.VisibleForTesting;
@@ -90,13 +95,15 @@ public class RequestAuthorizeSignatureV2 implements RequestAuthorizeSignature, R
    private final String headerTag;
    private final String servicePath;
    private final boolean isVhostStyle;
+   private final DateService dateService;
 
    @Inject
    public RequestAuthorizeSignatureV2(SignatureWire signatureWire, @Named(PROPERTY_AUTH_TAG) String authTag,
          @Named(PROPERTY_S3_VIRTUAL_HOST_BUCKETS) boolean isVhostStyle,
          @Named(PROPERTY_S3_SERVICE_PATH) String servicePath, @Named(PROPERTY_HEADER_TAG) String headerTag,
          @org.jclouds.location.Provider Supplier<Credentials> creds,
-         @TimeStamp Provider<String> timeStampProvider, Crypto crypto, HttpUtils utils) {
+         @TimeStamp Provider<String> timeStampProvider, Crypto crypto, HttpUtils utils,
+         DateService dateService) {
       this.isVhostStyle = isVhostStyle;
       this.servicePath = servicePath;
       this.headerTag = headerTag;
@@ -106,6 +113,7 @@ public class RequestAuthorizeSignatureV2 implements RequestAuthorizeSignature, R
       this.timeStampProvider = timeStampProvider;
       this.crypto = crypto;
       this.utils = utils;
+      this.dateService = dateService;
    }
 
    public HttpRequest filter(HttpRequest request) throws HttpException {
@@ -260,5 +268,33 @@ public class RequestAuthorizeSignatureV2 implements RequestAuthorizeSignature, R
             separator = '&';
          }
       }
+   }
+
+   @Override
+   public HttpRequest signForTemporaryAccess(HttpRequest request, long timeInSeconds) {
+      // Update the 'DATE' header
+      String dateString = request.getFirstHeaderOrNull(HttpHeaders.DATE);
+      if (dateString == null) {
+         dateString = timeStampProvider.get();
+      }
+      Date date = dateService.rfc1123DateParse(dateString);
+      String expiration = String.valueOf(TimeUnit.MILLISECONDS.toSeconds(date.getTime()) + timeInSeconds);
+      HttpRequest.Builder<?> builder = request.toBuilder()
+         .removeHeader(HttpHeaders.AUTHORIZATION)
+         .replaceHeader(HttpHeaders.DATE, expiration);
+      String stringToSign = createStringToSign(builder.build());
+      String signature = sign(stringToSign);
+      HttpRequest ret = builder
+         .addQueryParam(HttpHeaders.EXPIRES, expiration)
+         .addQueryParam("AWSAccessKeyId", creds.get().identity)
+         // Signature MUST be the last parameter because if it isn't, even encoded '+' values in the
+         // signature will be converted to a space by a subsequent addQueryParameter.
+         // See HttpRequestTest.testAddBase64AndUrlEncodedQueryParams for more details.
+         .addQueryParam(S3Constants.TEMPORARY_SIGNATURE_PARAM, signature)
+         // remove signer created by RestAnnotationProcessor
+         .removeHeader(HttpHeaders.DATE)
+         .filters(ImmutableList.<HttpRequestFilter>of())
+         .build();
+      return ret;
    }
 }

apis/s3/src/main/java/org/jclouds/s3/filters/RequestAuthorizeSignature.java
@@ -16,6 +16,7 @@
  */
 package org.jclouds.s3.filters;
 
+import org.jclouds.http.HttpRequest;
 import org.jclouds.http.HttpRequestFilter;
 
 /**
@@ -23,4 +24,5 @@ import org.jclouds.http.HttpRequestFilter;
  */
 
 public interface RequestAuthorizeSignature extends HttpRequestFilter {
+   HttpRequest signForTemporaryAccess(HttpRequest request, long timeInSeconds);
 }

apis/s3/src/main/java/org/jclouds/s3/filters/RequestAuthorizeSignatureV4.java
@@ -106,6 +106,7 @@ public class RequestAuthorizeSignatureV4 implements RequestAuthorizeSignature {
     * For example, you might store videos in an Amazon S3 bucket and make them available on your website by using presigned URLs.
     * Identifies the version of AWS Signature and the algorithm that you used to calculate the signature.
     */
+   @Override
    public HttpRequest signForTemporaryAccess(HttpRequest request, long timeInSeconds) {
       return signerForQueryString.sign(request, timeInSeconds);
    }

apis/s3/src/main/java/org/jclouds/s3/reference/S3Constants.java
@@ -33,6 +33,8 @@ public final class S3Constants {
    public static final String PROPERTY_S3_VIRTUAL_HOST_BUCKETS = "jclouds.s3.virtual-host-buckets";
    public static final String PROPERTY_JCLOUDS_S3_CHUNKED_SIZE = "jclouds.s3.chunked.size";
 
+   public static final String TEMPORARY_SIGNATURE_PARAM = "Signature";
+
    private S3Constants() {
       throw new AssertionError("intentionally unimplemented");
    }

apis/s3/src/test/java/org/jclouds/s3/blobstore/integration/S3BlobSignerLiveTest.java
@@ -16,13 +16,10 @@
  */
 package org.jclouds.s3.blobstore.integration;
 
-import static org.testng.Assert.fail;
-
 import java.io.IOException;
 
 import org.jclouds.blobstore.integration.internal.BaseBlobSignerLiveTest;
 import org.jclouds.blobstore.integration.internal.BaseBlobStoreIntegrationTest;
-import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 @Test(groups = "live", testName = "S3BlobSignerLiveTest")
@@ -33,59 +30,17 @@ public class S3BlobSignerLiveTest extends BaseBlobSignerLiveTest {
       BaseBlobStoreIntegrationTest.SANITY_CHECK_RETURNED_BUCKET_NAME = true;
    }
 
-   protected boolean supportsUrlWithTime() {
-      return false;
-   }
-
-   @Test
-   public void testSignGetUrlWithTime() throws InterruptedException, IOException {
-      try {
-         super.testSignGetUrlWithTime();
-         if (!supportsUrlWithTime()) {
-            fail();
-         }
-      } catch (UnsupportedOperationException uoe) {
-         throw new SkipException("not supported by S3 signer", uoe);
-      }
-   }
-
    @Test
    public void testSignGetUrlWithTimeExpired() throws InterruptedException, IOException {
-      try {
-         // Intentionally try with a timeout of 0. AWS signature v4 throws an error if
-         // the timeout is negative.
-         super.testSignGetUrlWithTime(/*timeout=*/ 0);
-         if (!supportsUrlWithTime()) {
-            fail();
-         }
-      } catch (UnsupportedOperationException uoe) {
-         throw new SkipException("not supported by S3 signer", uoe);
-      }
-   }
-
-   @Test
-   public void testSignPutUrlWithTime() throws Exception {
-      try {
-         super.testSignPutUrlWithTime();
-         if (!supportsUrlWithTime()) {
-            fail();
-         }
-      } catch (UnsupportedOperationException uoe) {
-         throw new SkipException("not supported by S3 signer", uoe);
-      }
+      // Intentionally try with a timeout of 0. AWS signature v4 throws an error if
+      // the timeout is negative.
+      super.testSignGetUrlWithTime(/*timeout=*/ 0);
    }
 
    @Test
    public void testSignPutUrlWithTimeExpired() throws Exception {
-      try {
-         // Intentionally try with a timeout of 0. AWS signature v4 throws an error if
-         // the timeout is negative.
-         super.testSignPutUrlWithTime(/*timeout=*/ 0);
-         if (!supportsUrlWithTime()) {
-            fail();
-         }
-      } catch (UnsupportedOperationException uoe) {
-         throw new SkipException("not supported by S3 signer", uoe);
-      }
+      // Intentionally try with a timeout of 0. AWS signature v4 throws an error if
+      // the timeout is negative.
+      super.testSignPutUrlWithTime(/*timeout=*/ 0);
    }
 }

providers/aws-s3/src/main/java/org/jclouds/aws/s3/blobstore/AWSS3BlobRequestSigner.java
@@ -1,102 +1,0 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jclouds.aws.s3.blobstore;
-
-import static com.google.common.base.Preconditions.checkNotNull;
-import static org.jclouds.blobstore.util.BlobStoreUtils.cleanRequest;
-
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
-
-import org.jclouds.aws.s3.AWSS3Client;
-import org.jclouds.blobstore.domain.Blob;
-import org.jclouds.blobstore.functions.BlobToHttpGetOptions;
-import org.jclouds.date.DateService;
-import org.jclouds.date.TimeStamp;
-import org.jclouds.domain.Credentials;
-import org.jclouds.http.HttpRequest;
-import org.jclouds.reflect.Invocation;
-import org.jclouds.rest.internal.RestAnnotationProcessor;
-import org.jclouds.s3.blobstore.S3BlobRequestSigner;
-import org.jclouds.s3.blobstore.functions.BlobToObject;
-import org.jclouds.s3.filters.RequestAuthorizeSignatureV2;
-
-import com.google.common.base.Supplier;
-import com.google.common.collect.ImmutableList;
-import com.google.common.net.HttpHeaders;
-import com.google.inject.Inject;
-import com.google.inject.Provider;
-
-public class AWSS3BlobRequestSigner extends S3BlobRequestSigner<AWSS3Client> {
-   public static final String TEMPORARY_SIGNATURE_PARAM = "Signature";
-
-   private final RequestAuthorizeSignatureV2 authSigner;
-   private final String identity;
-   private final DateService dateService;
-   private final Provider<String> timeStampProvider;
-
-   @Inject
-   public AWSS3BlobRequestSigner(RestAnnotationProcessor processor, BlobToObject blobToObject,
-         BlobToHttpGetOptions blob2HttpGetOptions, Class<AWSS3Client> interfaceClass,
-         @org.jclouds.location.Provider Supplier<Credentials> credentials,
-         RequestAuthorizeSignatureV2 authSigner, @TimeStamp Provider<String> timeStampProvider,
-         DateService dateService) throws SecurityException, NoSuchMethodException {
-      super(processor, blobToObject, blob2HttpGetOptions, interfaceClass);
-      this.authSigner = authSigner;
-      this.identity = credentials.get().identity;
-      this.dateService = dateService;
-      this.timeStampProvider = timeStampProvider;
-   }
-
-   @Override
-   public HttpRequest signGetBlob(String container, String name, long timeInSeconds) {
-      checkNotNull(container, "container");
-      checkNotNull(name, "name");
-      HttpRequest request = processor.apply(Invocation.create(getMethod, ImmutableList.<Object> of(container, name)));
-      return cleanRequest(signForTemporaryAccess(request, timeInSeconds));
-   }
-
-   @Override
-   public HttpRequest signPutBlob(String container, Blob blob, long timeInSeconds) {
-      checkNotNull(container, "container");
-      checkNotNull(blob, "blob");
-      HttpRequest request = processor.apply(Invocation.create(createMethod,
-         ImmutableList.<Object>of(container, blobToObject.apply(blob))));
-      return cleanRequest(signForTemporaryAccess(request, timeInSeconds));
-   }
-
-   private HttpRequest signForTemporaryAccess(HttpRequest request, long timeInSeconds) {
-      // Update the 'DATE' header
-      String dateString = request.getFirstHeaderOrNull(HttpHeaders.DATE);
-      if (dateString == null) {
-         dateString = timeStampProvider.get();
-      }
-      Date date = dateService.rfc1123DateParse(dateString);
-      String expiration = String.valueOf(TimeUnit.MILLISECONDS.toSeconds(date.getTime()) + timeInSeconds);
-      HttpRequest.Builder<?> builder = request.toBuilder().replaceHeader(HttpHeaders.DATE, expiration);
-      String stringToSign = authSigner.createStringToSign(builder.build());
-      String signature = authSigner.sign(stringToSign);
-      HttpRequest ret = builder.addQueryParam(HttpHeaders.EXPIRES, expiration)
-         .addQueryParam("AWSAccessKeyId", identity)
-         // Signature MUST be the last parameter because if it isn't, even encoded '+' values in the
-         // signature will be converted to a space by a subsequent addQueryParameter.
-         // See HttpRequestTest.testAddBase64AndUrlEncodedQueryParams for more details.
-         .addQueryParam(TEMPORARY_SIGNATURE_PARAM, signature)
-         .build();
-      return ret;
-   }
-}

commit db00f679ef32831bc65bcea02ecb05bc5330a5b9

JCLOUDS-255: Consistently use query string auth
Fixes regressions introduced in
7a110b31ba7a7bd68c9c180a922391a46ff5510e.

apis/s3/src/main/java/org/jclouds/s3/blobstore/S3BlobRequestSigner.java
@@ -41,6 +41,9 @@ import com.google.common.reflect.Invokable;
 
 @Singleton
 public class S3BlobRequestSigner<T extends S3Client> implements BlobRequestSigner {
+   /** Matches Amazon default when Expiry parameter not present. */
+   private static final int DEFAULT_EXPIRY_SECONDS = 15 * 60;
+
    private final RequestAuthorizeSignature authSigner;
 
    protected final RestAnnotationProcessor processor;
@@ -67,9 +70,7 @@ public class S3BlobRequestSigner<T extends S3Client> implements BlobRequestSigne
 
    @Override
    public HttpRequest signGetBlob(String container, String name) {
-      checkNotNull(container, "container");
-      checkNotNull(name, "name");
-      return cleanRequest(processor.apply(Invocation.create(getMethod, ImmutableList.<Object> of(container, name))));
+      return signGetBlob(container, name, DEFAULT_EXPIRY_SECONDS);
    }
 
    @Override
@@ -82,10 +83,7 @@ public class S3BlobRequestSigner<T extends S3Client> implements BlobRequestSigne
 
    @Override
    public HttpRequest signPutBlob(String container, Blob blob) {
-      checkNotNull(container, "container");
-      checkNotNull(blob, "blob");
-      return cleanRequest(processor.apply(Invocation.create(createMethod,
-            ImmutableList.<Object> of(container, blobToObject.apply(blob)))));
+      return signPutBlob(container, blob, DEFAULT_EXPIRY_SECONDS);
    }
 
    @Override

apis/s3/src/main/java/org/jclouds/s3/filters/RequestAuthorizeSignatureV2.java
@@ -291,8 +291,8 @@ public class RequestAuthorizeSignatureV2 implements RequestAuthorizeSignature, R
          // signature will be converted to a space by a subsequent addQueryParameter.
          // See HttpRequestTest.testAddBase64AndUrlEncodedQueryParams for more details.
          .addQueryParam(S3Constants.TEMPORARY_SIGNATURE_PARAM, signature)
-         // remove signer created by RestAnnotationProcessor
          .removeHeader(HttpHeaders.DATE)
+         // remove signer created by RestAnnotationProcessor
          .filters(ImmutableList.<HttpRequestFilter>of())
          .build();
       return ret;

apis/s3/src/test/java/org/jclouds/s3/blobstore/S3BlobSignerExpectTest.java
@@ -22,7 +22,6 @@ import org.jclouds.http.HttpRequest;
 import org.jclouds.rest.ConfiguresHttpApi;
 import org.jclouds.s3.S3Client;
 import org.jclouds.s3.config.S3HttpApiModule;
-import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import com.google.common.base.Supplier;
@@ -42,23 +41,20 @@ public class S3BlobSignerExpectTest extends BaseBlobSignerExpectTest {
    protected HttpRequest getBlob() {
       return HttpRequest.builder().method("GET")
                         .endpoint("http://localhost/container/name")
-                        .addHeader("Date", "Thu, 05 Jun 2008 16:38:19 GMT")
-                        .addHeader("Authorization", "AWS identity:0uvBv1wEskuhFHYJF/L6kEV9A7o=").build();
+                        .addQueryParam("Expires", "1212684799")
+                        .addQueryParam("AWSAccessKeyId", "identity")
+                        .addQueryParam("Signature", "Z0UqrkQv83rykFxvI3f0dQtxEAM=")
+                        .build();
    }
 
-   @Override
-   @Test
-   public void testSignGetBlobWithTime() {
-      throw new SkipException("not yet implemented");
-   }
-
-   //TODO
    @Override
    protected HttpRequest getBlobWithTime() {
       return HttpRequest.builder().method("GET")
-            .endpoint("http://locahost/container/name")
-            .addHeader("Date", "Thu, 05 Jun 2008 16:38:19 GMT")
-            .addHeader("Authorization", "AWS identity:0uvBv1wEskuhFHYJF/L6kEV9A7o=").build();
+            .endpoint("http://localhost/container/name")
+            .addQueryParam("Expires", "1212683902")
+            .addQueryParam("AWSAccessKeyId", "identity")
+            .addQueryParam("Signature", "Y4Ac4sZfBemGZmgfG78F7IX+IFg=")
+            .build();
    }
 
    @Override
@@ -74,25 +70,22 @@ public class S3BlobSignerExpectTest extends BaseBlobSignerExpectTest {
    protected HttpRequest putBlob() {
       return HttpRequest.builder().method("PUT")
             .endpoint("http://localhost/container/name")
+            .addQueryParam("Expires", "1212684799")
+            .addQueryParam("AWSAccessKeyId", "identity")
+            .addQueryParam("Signature", "N3+nS6ogzOqgT+YaThFN6RU/+xs=")
             .addHeader("Expect", "100-continue")
-            .addHeader("Date", "Thu, 05 Jun 2008 16:38:19 GMT")
-            .addHeader("Authorization", "AWS identity:zM2oT+71KcoOSxv1SU5L12UXnT8=").build();
+            .build();
    }
 
-   @Override
-   @Test
-   public void testSignPutBlobWithTime() throws Exception {
-      throw new SkipException("not yet implemented");
-   }
-
-   //TODO
    @Override
    protected HttpRequest putBlobWithTime() {
       return HttpRequest.builder().method("PUT")
             .endpoint("http://localhost/container/name")
+            .addQueryParam("Expires", "1212683902")
+            .addQueryParam("AWSAccessKeyId", "identity")
+            .addQueryParam("Signature", "genkB2vLxe3AWV/bPvRTMqQts7E=")
             .addHeader("Expect", "100-continue")
-            .addHeader("Date", "Thu, 05 Jun 2008 16:38:19 GMT")
-            .addHeader("Authorization", "AWS identity:zM2oT+71KcoOSxv1SU5L12UXnT8=").build();
+            .build();
    }
 
    @Override

providers/aws-s3/src/test/java/org/jclouds/aws/s3/blobstore/AWSS3BlobSignerExpectTest.java
@@ -18,12 +18,17 @@ package org.jclouds.aws.s3.blobstore;
 
 import static org.testng.Assert.assertEquals;
 
+import java.util.Date;
 import java.util.Map;
 
+import javax.inject.Named;
+
+import org.jclouds.Constants;
 import org.jclouds.aws.s3.config.AWSS3HttpApiModule;
 import org.jclouds.aws.s3.filters.AWSRequestAuthorizeSignature;
 import org.jclouds.blobstore.BlobStore;
 import org.jclouds.blobstore.domain.Blob;
+import org.jclouds.date.DateService;
 import org.jclouds.date.TimeStamp;
 import org.jclouds.http.HttpRequest;
 import org.jclouds.rest.ConfiguresHttpApi;
@@ -33,6 +38,7 @@ import org.testng.annotations.Test;
 
 import com.google.common.base.Splitter;
 import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 import com.google.inject.Module;
 import com.google.inject.Scopes;
 
@@ -49,9 +55,14 @@ public class AWSS3BlobSignerExpectTest extends S3BlobSignerExpectTest {
    protected HttpRequest getBlob() {
       return HttpRequest.builder().method("GET")
             .endpoint("https://container.s3.amazonaws.com/name")
+            .addQueryParam("X-Amz-Algorithm", "AWS4-HMAC-SHA256")
+            .addQueryParam("X-Amz-Credential", "identity/20080605/us-east-1/s3/aws4_request")
+            .addQueryParam("X-Amz-Date", "20080605T163819Z")
+            .addQueryParam("X-Amz-Expires", "900")
+            .addQueryParam("X-Amz-SignedHeaders", "host")
+            .addQueryParam("X-Amz-Signature", "1aa13b18ef9c4a9a98db7539e9eeb2c63afadbab649e14e28d5b765dfd96c32b")
             .addHeader("Host", HOST)
-            .addHeader("Date", "Thu, 05 Jun 2008 16:38:19 GMT")
-            .addHeader("Authorization", "AWS identity:0uvBv1wEskuhFHYJF/L6kEV9A7o=").build();
+            .build();
    }
 
    @Override
@@ -71,8 +82,10 @@ public class AWSS3BlobSignerExpectTest extends S3BlobSignerExpectTest {
             .endpoint("https://container.s3.amazonaws.com/name")
             .addHeader("Host", HOST)
             .addHeader("Range", "bytes=0-1")
-            .addHeader("Date", "Thu, 05 Jun 2008 16:38:19 GMT")
-            .addHeader("Authorization", "AWS identity:0uvBv1wEskuhFHYJF/L6kEV9A7o=").build();
+            .addHeader("x-amz-content-sha256", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
+            .addHeader("X-Amz-Date", "20080605T163819Z")
+            .addHeader("Authorization", "AWS4-HMAC-SHA256 Credential=identity/20080605/us-east-1/s3/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date, Signature=8f6a70bf43f31c92a67095510b080f574154df8a5ccb988ec8a6cbcce03dd5b8")
+            .build();
    }
 
    private void compareRequestComponents(final HttpRequest request, final HttpRequest compare) {
@@ -80,15 +93,16 @@ public class AWSS3BlobSignerExpectTest extends S3BlobSignerExpectTest {
       String query = request.getEndpoint().toString().split("\\?")[1];
       final Map<String, String> params = Splitter.on('&').trimResults().withKeyValueSeparator("=").split(query);
       assertEquals(params.get("X-Amz-Algorithm"), "AWS4-HMAC-SHA256");
-      assertEquals(params.get("X-Amz-Expires"), "3");
+      assertEquals(params.get("X-Amz-Expires"), "900");
       assertEquals(params.get("X-Amz-SignedHeaders"), "host");
    }
 
+   @Override
    @Test
    public void testSignGetBlobWithTime() {
       BlobStore getBlobWithTime = requestsSendResponses(init());
       HttpRequest compare = getBlobWithTime();
-      HttpRequest request = getBlobWithTime.getContext().getSigner().signGetBlob(container, name, 3L /* seconds */);
+      HttpRequest request = getBlobWithTime.getContext().getSigner().signGetBlob(container, name, 900L /* seconds */);
       compareRequestComponents(request, compare);
    }
 
@@ -119,18 +133,33 @@ public class AWSS3BlobSignerExpectTest extends S3BlobSignerExpectTest {
    protected HttpRequest removeBlob() {
       return HttpRequest.builder().method("DELETE")
             .endpoint("https://container.s3.amazonaws.com/name")
+            .addHeader("x-amz-content-sha256", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
+            .addHeader("X-Amz-Date", "20080605T163819Z")
+            .addHeader("Authorization", "AWS4-HMAC-SHA256 Credential=identity/20080605/us-east-1/s3/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date, Signature=b068a3b2a76f06bf1d73b907243602f43962f5572ea1e588ed193c8c656118fe")
             .addHeader("Host", HOST)
-            .addHeader("Date", "Thu, 05 Jun 2008 16:38:19 GMT")
-            .addHeader("Authorization", "AWS identity:4FnyjdX/ULdDMRbVlLNjZfEo9RQ=").build();
+            .build();
    }
 
+   @Override
+   @Test
+   public void testSignPutBlob() throws Exception {
+      BlobStore signPutBloblWithTime = requestsSendResponses(init());
+      Blob blob = signPutBloblWithTime.blobBuilder(name).payload(text).contentType("text/plain").build();
+      HttpRequest compare = putBlobWithTime();
+      compare.setPayload(blob.getPayload());
+      HttpRequest request = signPutBloblWithTime.getContext().getSigner().signPutBlob(container, blob);
+      compareRequestComponents(request, compare);
+      assertEquals(request.getPayload(), compare.getPayload());
+   }
+
+   @Override
    @Test
    public void testSignPutBlobWithTime() throws Exception {
       BlobStore signPutBloblWithTime = requestsSendResponses(init());
       Blob blob = signPutBloblWithTime.blobBuilder(name).payload(text).contentType("text/plain").build();
       HttpRequest compare = putBlobWithTime();
       compare.setPayload(blob.getPayload());
-      HttpRequest request = signPutBloblWithTime.getContext().getSigner().signPutBlob(container, blob, 3L /* seconds */);
+      HttpRequest request = signPutBloblWithTime.getContext().getSigner().signPutBlob(container, blob, 900L /* seconds */);
       compareRequestComponents(request, compare);
       assertEquals(request.getPayload(), compare.getPayload());
    }
@@ -148,10 +177,13 @@ public class AWSS3BlobSignerExpectTest extends S3BlobSignerExpectTest {
          return DATE;
       }
 
-      // subclass expects v2 signatures
       @Override
-      protected void bindRequestSigner() {
-         bind(RequestAuthorizeSignature.class).to(AWSRequestAuthorizeSignature.class).in(Scopes.SINGLETON);
+      @TimeStamp
+      protected Supplier<Date> provideTimeStampCacheDate(
+            @Named(Constants.PROPERTY_SESSION_INTERVAL) long seconds,
+            @TimeStamp final Supplier<String> timestamp,
+            final DateService dateService) {
+         return Suppliers.ofInstance(new Date(1212683899000L));
       }
    }
 }