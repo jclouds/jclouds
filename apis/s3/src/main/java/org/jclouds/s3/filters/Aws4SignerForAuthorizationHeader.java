36b3dadf8c3f817c80f7edc639b449a2b40b9388
Stephen Tomkinson <stephen.tomkinson@gmail.com>
2017-05-10 11:41:23 +0100

    Add logic from JCLOUDS-1261 to the other 2 types of Aws4 signer.

--- apis/s3/src/main/java/org/jclouds/s3/filters/Aws4SignerForAuthorizationHeader.java
+++ apis/s3/src/main/java/org/jclouds/s3/filters/Aws4SignerForAuthorizationHeader.java
@@ -75,7 +75,6 @@ public class Aws4SignerForAuthorizationHeader extends Aws4SignerBase {
 
       // get host & port from request endpoint.
       String host = request.getEndpoint().getHost();
-      int port = request.getEndpoint().getPort();
 
       Date date = timestampProvider.get();
       String timestamp = timestampFormat.format(date);
@@ -120,13 +119,7 @@ public class Aws4SignerForAuthorizationHeader extends Aws4SignerBase {
       }
 
       // host
-      // if the port is defined and doesn't match the URI scheme
-      if (port != -1) {
-         if (("http".equalsIgnoreCase(request.getEndpoint().getScheme()) && port != 80) ||
-                 ("https".equalsIgnoreCase(request.getEndpoint().getScheme()) && port != 443)) {
-            host += ":" + port; // append the port number to the hostname
-         }
-      }
+      host = hostHeaderFor(request.getEndpoint());
       requestBuilder.replaceHeader(HttpHeaders.HOST, host);
       signedHeadersBuilder.put(HttpHeaders.HOST.toLowerCase(), host);
 
